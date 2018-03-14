f489089d5b67e439ccc41f60fe77332ea1a056ae
Edward Buckler <esb33@cornell.edu>
2018-03-14 17:00:43 -0400

    Improved documentation and using this and super

--- CSE114/Rand/TestClasses/Animal.java
+++ CSE114/Rand/TestClasses/Animal.java
@@ -1,5 +1,6 @@
 /**
  * Example of interheritance and usage of this, super
+ * @author edbuckler
  */
 public class Animal {
     int legs;
@@ -9,13 +10,21 @@ public class Animal {
         this(legs,"Unknown");
     }
 
+    /**
+     * Generic constructor for
+     * @param legs number of legs
+     * @param fur what type of fur
+     */
     public Animal(int legs, String fur) {
         this.legs = legs;
         this.fur = fur;
+        System.out.println(toString());
     }
 
-    @Override
+    //@Override
     public String toString() {
+        System.out.println("this = " + this.hashCode());
+        System.out.println(super.toString());
         return "AnimalInherit{" +
                 "legs=" + legs +
                 ", fur='" + fur + '\'' +
