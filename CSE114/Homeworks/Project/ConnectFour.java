9b9810a469345a0b5c30579866a22a5ddc0fd8bb
Edward Buckler <esbv98@gmail.com>
2018-05-05 05:28:02 -0400

    Going for brute force tactic
    Small glitch left

--- CSE114/Homeworks/Project/ConnectFour.java
+++ CSE114/Homeworks/Project/ConnectFour.java
@@ -1,4 +1,9 @@
+import com.sun.org.apache.xerces.internal.impl.dv.xs.IntegerDV;
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -50,7 +55,8 @@ public class ConnectFour {
                     board.printBoard();
                 }
                 //test if win using lowestEmptySlot + 1 to get value of just dropped checker
-                if (board.testIfWin((board.lowestEmptySpot(val) == -1) ? 0 : board.lowestEmptySpot(val) + 1, val, 'R')) {
+//                if (board.testIfWin((board.lowestEmptySpot(val) == -1) ? 0 : board.lowestEmptySpot(val) + 1, val, 'R')) {
+                if (board.testIfWin()) {
                     System.out.println("The red player won.");
                     win = true;
                 }
@@ -67,7 +73,8 @@ public class ConnectFour {
                     board.printBoard();
                 }
                 //test if win using lowestEmptySlot + 1 to get value of just dropped checker
-                if (board.testIfWin((board.lowestEmptySpot(val) == -1) ? 0 : board.lowestEmptySpot(val) + 1, val, 'Y')) {
+                //if (board.testIfWin((board.lowestEmptySpot(val) == -1) ? 0 : board.lowestEmptySpot(val) + 1, val, 'Y')) {
+                if (board.testIfWin()) {
                     System.out.println("The yellow player won.");
                     win = true;
                 }
@@ -131,8 +138,9 @@ class GameBoard {
      * prints board to console
      */
     public void printBoard() {
+        System.out.println("   0 1 2 3 4 5 6");
         for (int i = 0; i < board.length; i++) {
-            System.out.print("|");
+            System.out.print(i + " |");
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j].getType() + "|");
             }
@@ -188,70 +196,129 @@ class GameBoard {
         return true;
     }
 
-    /**
-     * PROBLEM IS PROBABLY HERE!!! Realistically I overlooked something with how stacks work
-     *
-     * @param row
-     * @param column
-     * @param adjacent
-     * @param direction
-     * @param type
-     * @return How many checkers have been found in a row
-     * row, column designate origin
-     * adjacent designates how many in a row have been found start with 0
-     * direction forces which direction to look to -1 if search all
-     * Direction:
-     * 1 2 3
-     * 8 X 4
-     * 7 6 5
-     */
-    private int testNextToo(int row, int column, int adjacent, int direction, char type) {
-        if (direction == -1) {
-            //iterate through all 8 directions
-            for (int i = 1; i <= 8; i++) {
-                int[] directionCoord = directionTranslate(row, column, i);
-                //if direction is not out of bounds
-                if (directionCoord[0] != -1) {
-                    // if checker at target pos is of same type
-                    if (board[directionCoord[0]][directionCoord[1]].getType() == type) {
-                        //temp call out
-                        System.out.println(adjacent + ": REC 1 detection " + board[directionCoord[0]][directionCoord[1]].getType() + " at " + i + " (" + directionCoord[0] + ", " + directionCoord[1] + ")" + " rel to (" + row + ", " + column + ")");
-                        System.out.println("Rec 1 Call: row " + directionCoord[0] + ", column " + directionCoord[1] +
-                                ", adjacent " + (adjacent + 1) + ", direction " + i + ", type " + type);
-                        //If checker same type call method again but with that checker as origin
-                        testNextToo(directionCoord[0], directionCoord[1], ++adjacent, i, type);
-                    }
-                }
-            }
-        } else {
-            int[] directionCoord = directionTranslate(row, column, direction);
-            if (directionCoord[0] != -1) {
-                if (board[directionCoord[0]][directionCoord[1]].getType() == type) {
-                    //temp call out
-                    System.out.println(adjacent + ": REC 2 detection " + board[directionCoord[0]][directionCoord[1]].getType() + " at " + direction + " (" + directionCoord[0] + ", " + directionCoord[1] + ")" + " rel to (" + row + ", " + column + ")");
-                    System.out.println("Rec 2 Call: row " + directionCoord[0] + ", column " + directionCoord[1] + ", " +
-                            "adjacent " + (adjacent + 1) + ", direction " + direction + ", type " + type);
-                    //If checker same type call method again but with that checker as origin
-                    testNextToo(directionCoord[0], directionCoord[1], ++adjacent, direction, type);
-                }
-            }
-        }
-        return adjacent;
-    }
+//    /**
+//     * Cool Way
+//     * PROBLEM IS PROBABLY HERE!!! Realistically I overlooked something with how stacks work
+//     *
+//     * @param row
+//     * @param column
+//     * @param adjacent
+//     * @param direction
+//     * @param type
+//     * @return How many checkers have been found in a row
+//     * row, column designate origin
+//     * adjacent designates how many in a row have been found start with 0
+//     * direction forces which direction to look to -1 if search all
+//     * Direction:
+//     * 1 2 3
+//     * 8 X 4
+//     * 7 6 5
+//     */
+//    private int testNextToo(int row, int column, int adjacent, int direction, char type) {
+//        if (direction == -1) {
+//            //iterate through all 8 directions
+//            for (int i = 1; i <= 8; i++) {
+//                ArrayList<Integer[]> temp = new ArrayList<>();
+//                Integer[] tempDirCoord = directionTranslate(row, column, i);
+//                if (tempDirCoord[0] != -1) {
+//                    if (board[tempDirCoord[0]][tempDirCoord[1]].getType() == type) {
+//                        System.out.println("got here1");
+//                        temp.add(tempDirCoord);
+//                    }
+//                    for (int j = 0; j < temp.size(); j++) {
+//                        //temp call out
+//                        System.out.println(adjacent + ": REC 1 detection " + board[temp.get(j)[0]][temp.get(j)[1]].getType()
+//                                + " at " + temp.get(j)[2] + " (" + temp.get(j)[0] + ", " + temp.get(j)[1] + ")" + " rel to (" + row + ", " + column + ")");
+//                        System.out.println("Rec 1 Call: row " + temp.get(j)[0] + ", column " + temp.get(j)[1] + ", " +
+//                                "adjacent " + (adjacent + 1) + ", direction " + temp.get(j)[2] + ", type " + type);
+//                        return testNextToo(temp.get(j)[0], temp.get(j)[1], ++adjacent, i, type);
+//                    }
+//                }
+//                if (i == 8 && temp.size() == 0) {
+//                    return adjacent;
+//                }
+//            }
+//        } else {
+//            Integer[] directionCoord = directionTranslate(row, column, direction);
+//            if (directionCoord[0] != -1) {
+//                if (board[directionCoord[0]][directionCoord[1]].getType() == type) {
+//                    //temp call out
+//                    System.out.println(adjacent + ": REC 2 detection " + board[directionCoord[0]][directionCoord[1]].getType()
+//                            + " at " + direction + " (" + directionCoord[0] + ", " + directionCoord[1] + ")" + " rel to (" + row + ", " + column + ")");
+//                    System.out.println("Rec 2 Call: row " + directionCoord[0] + ", column " + directionCoord[1] + ", " +
+//                            "adjacent " + (adjacent + 1) + ", direction " + direction + ", type " + type);
+//                    //If checker same type call method again but with that checker as origin
+//                    return testNextToo(directionCoord[0], directionCoord[1], ++adjacent, direction, type);
+//                }
+//            }
+//        }
+//        return adjacent;
+//    }
+//    /**
+//     * @param row
+//     * @param column
+//     * @param type
+//     * @return bool based on if on side has won
+//     */
+//    public boolean testIfWin(int row, int column, char type) {
+//        int val = testNextToo(row, column, 0, -1, type);
+//        //temp call out
+//        System.out.println("val = " + val);
+//        if (val >= 3) {
+//            return true;
+//        }
+//        return false;
+//    }
 
-    /**
-     * @param row
-     * @param column
-     * @param type
-     * @return bool based on if on side has won
-     */
-    public boolean testIfWin(int row, int column, char type) {
-        int val = testNextToo(row, column, 0, -1, type);
-        //temp call out
-        System.out.println("val = " + val);
-        if (val >= 3) {
-            return true;
+    public boolean testIfWin() {
+        //Vertical
+        for (int j = 0; j <= board[0].length - 1; j++) {
+            for (int i = 0; i <= board.length - 4; i++) {
+                if (board[i][j].getType() == board[i + 1][j].getType() &&
+                        board[i][j].getType() == board[i + 2][j].getType() &&
+                        board[i][j].getType() == board[i + 3][j].getType() &&
+                        board[i][j].getType() != ' ') {
+                    return true;
+                }
+            }
         }
+        //Horizontal
+        for (int j = 0; j <= board[0].length - 4; j++) {
+            for (int i = 0; i <= board.length - 1; i++) {
+                if (board[i][j].getType() == board[i][j + 1].getType() &&
+                        board[i][j].getType() == board[i][j + 2].getType() &&
+                        board[i][j].getType() == board[i][j + 3].getType() &&
+                        board[i][j].getType() != ' ') {
+                    return true;
+                }
+            }
+        }
+        // \
+        for (int j = 0; j <= board[0].length - 4; j++) {
+            for (int i = 0; i <= board.length - 4; i++) {
+                if (board[i][j].getType() == board[i + 1][j + 1].getType() &&
+                        board[i][j].getType() == board[i + 2][j + 2].getType() &&
+                        board[i][j].getType() == board[i + 3][j + 3].getType() &&
+                        board[i][j].getType() != ' ') {
+                    return true;
+                }
+            }
+        }
+        // /
+        for (int j = 3; j <= board[0].length - 1; j++) {
+            for (int i = 0; i <= board.length - 1; i++) {
+                if (board[i][j].getType() != ' ') {
+                    System.out.println("Type: " + board[i][j].getType() + " ("+i+", "+j+")");
+                }
+                if (board[i][j].getType() == board[i + 1][j - 1].getType() &&
+                        board[i][j].getType() == board[i + 2][j - 2].getType() &&
+                        board[i][j].getType() == board[i + 3][j - 3].getType() &&
+                        board[i][j].getType() != ' ') {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -269,25 +336,25 @@ class GameBoard {
      * (row, -1) (row, col) (row, +1)
      * (+1, -1) (+1, col) (+1, +1)
      */
-    private int[] directionTranslate(int row, int column, int direction) {
+    private Integer[] directionTranslate(int row, int column, int direction) {
         if (direction == 1 && row != 0 && column != 0) {
-            return new int[]{row - 1, column - 1};
+            return new Integer[]{row - 1, column - 1, direction};
         } else if (direction == 2 && row != 0) {
-            return new int[]{row - 1, column};
+            return new Integer[]{row - 1, column, direction};
         } else if (direction == 3 && row != 0 && column != board[0].length - 1) {
-            return new int[]{row - 1, column + 1};
+            return new Integer[]{row - 1, column + 1, direction};
         } else if (direction == 4 && column != board[0].length - 1) {
-            return new int[]{row, column + 1};
+            return new Integer[]{row, column + 1, direction};
         } else if (direction == 5 && column != board[0].length - 1 && row != board.length - 1) {
-            return new int[]{row + 1, column + 1};
+            return new Integer[]{row + 1, column + 1, direction};
         } else if (direction == 6 && row != board.length - 1 && row != board.length - 1) {
-            return new int[]{row + 1, column};
+            return new Integer[]{row + 1, column, direction};
         } else if (direction == 7 && row != board.length - 1 && column != 0) {
-            return new int[]{row + 1, column - 1};
+            return new Integer[]{row + 1, column - 1, direction};
         } else if (direction == 8 && column != 0) {
-            return new int[]{row, column - 1};
+            return new Integer[]{row, column - 1, direction};
         } else {
-            return new int[]{-1, -1};
+            return new Integer[]{-1, -1};
         }
     }
 }
--- CSE114/Rand/TestClasses/Nerd.java
+++ CSE114/Rand/TestClasses/Nerd.java
@@ -2,19 +2,8 @@ import java.util.Scanner;
 
 public class Nerd {
     public static void main(String[] args) {
-        int[] nerdArr = {1, 2, 3, 4, 5, 2, 3, 1, 3};
-        System.out.println(max(nerdArr, nerdArr[0], 0));
-    }
-
-    public static int max(int[] list, int max, int index) {
-        if (index == list.length) {
-            return max;
-        } else {
-            if (list[index] > max) {
-                return max(list, list[index], ++index);
-            } else {
-                return max(list, max, ++index);
-            }
-        }
+        Integer tester = 4;
+        tester++;
+        System.out.println(tester);
     }
 }
