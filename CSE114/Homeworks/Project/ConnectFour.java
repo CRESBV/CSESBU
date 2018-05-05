44e3099d64b0b29a50af01541d5189c89c12ed18
Edward Buckler V <esbv98@gmail.com>
2018-05-05 02:35:26 -0400

    Passing to other computer

--- CSE114/Homeworks/Project/ConnectFour.java
+++ CSE114/Homeworks/Project/ConnectFour.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.awt.*;
 import java.util.Scanner;
 
 /**
@@ -22,11 +22,13 @@ public class ConnectFour {
                     " 6): ");
             //read what was inputted
             String inputVal = input.nextLine();
+            int val;
             //New line for formatting
             System.out.println();
             //Clean input
             try {
-                if ((!(Integer.parseInt(inputVal) >= 0) && !(Integer.parseInt(inputVal) <= 6))) {
+                val = Integer.parseInt(inputVal);
+                if ((!(val >= 0) && !(val <= 6))) {
                     System.out.println("incorrect input");
                     continue;
                 }
@@ -37,25 +39,35 @@ public class ConnectFour {
                 continue;
             }
             //Alternator
-            //Red
+            //Red's turn
             if (redTurn) {
-                if (!board.placeChecker(Integer.parseInt(inputVal), new Checker('R')))
+                //if checker not placed continue
+                if (!board.placeChecker(val, new Checker('R'))) {
                     continue;
-                else
+                }
+                //if checker placed reprint board
+                else {
                     board.printBoard();
-                if (board.testIfWin(board.lowestEmptySpot(Integer.parseInt(inputVal)) + 1, Integer.parseInt(inputVal), 'R')) {
+                }
+                //test if win using lowestEmptySlot + 1 to get value of just dropped checker
+                if (board.testIfWin((board.lowestEmptySpot(val) == -1) ? 0 : board.lowestEmptySpot(val) + 1, val, 'R')) {
                     System.out.println("The red player won.");
                     win = true;
                 }
                 redTurn = false;
             }
-            //Yellow
+            //Yellow's turn
             else {
-                if (!board.placeChecker(Integer.parseInt(inputVal), new Checker('Y')))
+                //if checker not placed continue
+                if (!board.placeChecker(val, new Checker('Y'))) {
                     continue;
-                else
+                }
+                //if checker placed reprint board
+                else {
                     board.printBoard();
-                if (board.testIfWin(board.lowestEmptySpot(Integer.parseInt(inputVal)) + 1, Integer.parseInt(inputVal), 'Y')) {
+                }
+                //test if win using lowestEmptySlot + 1 to get value of just dropped checker
+                if (board.testIfWin((board.lowestEmptySpot(val) == -1) ? 0 : board.lowestEmptySpot(val) + 1, val, 'Y')) {
                     System.out.println("The yellow player won.");
                     win = true;
                 }
@@ -66,13 +78,10 @@ public class ConnectFour {
     }
 }
 
-/**
- * Direction:
- * 1 2 3
- * 8 X 4
- * 7 6 5
- */
 
+/**
+ * Checker object... used to have more stuff in it. Now just a char...
+ */
 class Checker {
     char type;
 
@@ -94,13 +103,22 @@ class Checker {
 }
 
 /**
- * board[row][column]
+ * Main methods class:
+ * Holds board data
+ * printing board
+ * empty slot in board
+ * placing new checkers
+ * finding adjacent checkers to one given
+ * discerning if one side has won
  */
-
 class GameBoard {
     static Checker[][] board = new Checker[6][7];
 
+    /**
+     * constructor
+     */
     public GameBoard() {
+        //init all checkers in board
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = new Checker();
@@ -108,6 +126,9 @@ class GameBoard {
         }
     }
 
+    /**
+     * prints board to console
+     */
     public void printBoard() {
         for (int i = 0; i < board.length; i++) {
             System.out.print("|");
@@ -116,9 +137,25 @@ class GameBoard {
             }
             System.out.print("\n");
         }
-        System.out.println("………………………………");
+        // Allows for full bar even on different screen resolutions
+        try {
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            System.out.println("screenSize.width = " + screenSize.width);
+            System.out.println("screenSize.height = " + screenSize.height);
+            if (screenSize.width == 1920) {
+                System.out.println("………………………………");
+            } else {
+                System.out.println("………………………………………");
+            }
+        } catch (final Exception e) {
+            System.out.println("………………………………");
+        }
     }
 
+    /**
+     * @param column
+     * @return Value of the lowest empty row in specified column
+     */
     public int lowestEmptySpot(int column) {
         try {
             for (int i = board.length - 1; i >= 0; i--) {
@@ -130,10 +167,15 @@ class GameBoard {
             System.out.println("Please enter int between 0-6");
         }
 
-        return 0;
+        return -1;
     }
 
 
+    /**
+     * @param column
+     * @param checker
+     * @return bool based on Whether or not a checker had been placed
+     */
     public boolean placeChecker(int column, Checker checker) {
         int lowestEmpty = lowestEmptySpot(column);
         if (lowestEmpty == -1) {
@@ -145,9 +187,20 @@ class GameBoard {
     }
 
     /**
+     * @param row
+     * @param column
+     * @param adjacent
+     * @param direction
+     * @param type
+     * @return How many checkers have been found in a row
      * row, column designate origin
      * adjacent designates how many in a row have been found start with 0
      * direction forces which direction to look to -1 if search all
+     * <p>
+     * Direction:
+     * 1 2 3
+     * 8 X 4
+     * 7 6 5
      */
     private int testNextToo(int row, int column, int adjacent, int direction, char type) {
         if (direction == -1) {
@@ -155,7 +208,10 @@ class GameBoard {
                 int[] directionCoord = directionTranslate(row, column, i);
                 if (directionCoord[0] != -1) {
                     if (board[directionCoord[0]][directionCoord[1]].getType() == type) {
-                        System.out.println(adjacent+": REC 1 detection "+type+" at "+ i + " ("+ directionCoord[0]+", "+ directionCoord[1] +")");
+                        //temp call out
+                        System.out.println(adjacent + ": REC 1 detection " + board[directionCoord[0]][directionCoord[1]].getType() + " at " + i + " (" + directionCoord[0] + ", " + directionCoord[1] + ")");
+                        System.out.println("Rec Call: row " + directionCoord[0] + ", column " + directionCoord[1] +
+                                ", adjacent " + ++adjacent + ", direction " + i + ", type " + type);
                         testNextToo(directionCoord[0], directionCoord[1], ++adjacent, i, type);
                     }
                 }
@@ -164,7 +220,8 @@ class GameBoard {
             int[] directionCoord = directionTranslate(row, column, direction);
             if (directionCoord[0] != -1) {
                 if (board[directionCoord[0]][directionCoord[1]].getType() == type) {
-                    System.out.println(adjacent+": REC 1 detection "+type+" at "+ direction + " ("+ directionCoord[0]+", "+ directionCoord[1] +")");
+                    //temp call out
+                    System.out.println(adjacent + ": REC 1 detection " + type + " at " + direction + " (" + directionCoord[0] + ", " + directionCoord[1] + ")");
                     testNextToo(directionCoord[0], directionCoord[1], ++adjacent, direction, type);
                 }
             }
@@ -172,8 +229,15 @@ class GameBoard {
         return adjacent;
     }
 
+    /**
+     * @param row
+     * @param column
+     * @param type
+     * @return bool based on if on side has won
+     */
     public boolean testIfWin(int row, int column, char type) {
         int val = testNextToo(row, column, 0, -1, type);
+        //temp call out
         System.out.println("val = " + val);
         if (val >= 3) {
             return true;
@@ -182,6 +246,10 @@ class GameBoard {
     }
 
     /**
+     * @param row
+     * @param column
+     * @param direction
+     * @return position on board based on origin and direction
      * Direction:
      * 1 2 3
      * 8 X 4
